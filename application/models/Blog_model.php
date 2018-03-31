52fc0f2d7bc3b682147d1218cd30b2d5aaae87b8
GONGTING520 <954160311@qq.com>
2018-03-31 17:13:07 +0800

    新增根据user_id查询文章信息，然后分页；增加查询此博客评论，查询博客的数据库方法

--- application/models/Blog_model.php
+++ application/models/Blog_model.php
@@ -22,7 +22,7 @@ class Blog_model extends CI_Model{
     }
 
     // 根据user_id查询文章信息
-    public function find_bolg_by_user_id($user_id){
+    public function find_blog_by_user_id($user_id){
         $this->db->select('b.*,bt.type_name');
         $this->db->from('t_blog b');
         $this->db->join('t_blog_type bt', 'b.type_id=bt.type_id');
@@ -30,9 +30,68 @@ class Blog_model extends CI_Model{
         return $this->db->get()->result();
     }
 
+    // 根据user_id查询文章信息，然后分页
+    public function pagination_blog_by_user_id($user_id, $offset, $page_length){
+        $this->db->select('b.*,bt.type_name');
+        $this->db->from('t_blog b');
+        $this->db->join('t_blog_type bt', 'b.type_id=bt.type_id');
+        $this->db->where('bt.user_id', $user_id);
+        $this->db->order_by('b.post_time', 'DESC');
+        $this->db->limit($page_length, $offset);
+        return $this->db->get()->result();
+    }
+
     // 根据blog_id删除一条博客
     public function delete_blog($blog_id){
         $this->db->delete('t_blog', array('blog_id' => $blog_id));
+        return $this->db->affected_rows();
+    }
+
+    // 根据blog_id查询一条博客
+    public function find_blog_by_blog_id($blog_id){
+        return $this->db->get_where('t_blog', array("blog_id"=>$blog_id))->row();
+    }
+
+    // 根据blog_id查询此博客的评论
+    public function find_comment_by_blog_id($blog_id){
+        $this->db->select('*');
+        $this->db->from('t_user u');
+        $this->db->join('t_comment c', 'u.user_id=c.user_id');
+        $this->db->where('c.blog_id', $blog_id);
+        $this->db->order_by('c.post_time', 'DESC');
+        return $this->db->get()->result();
+    }
+
+    // 查询所有的博客的数量
+    public function find_blog_user_blog_type_num(){
+        return $this->db->get('t_blog')->result();
+    }
+
+    // 查询所有的博客信息
+    public function pagination_blog_user_blog_type($offset, $page_length){
+        $this->db->select('*');
+        $this->db->from('t_blog b');
+        $this->db->join('t_blog_type bt', 'bt.type_id=b.type_id');
+        $this->db->join('t_user u', 'u.user_id=bt.user_id');
+        $this->db->order_by('b.post_time', 'DESC');
+        $this->db->limit($page_length, $offset);
+        return $this->db->get()->result();
+    }
+
+    // 根据user_id查询所有博客类型
+    public function find_blog_type_by_user_id($user_id){
+        $this->db->select('*,(SELECT COUNT(*) FROM t_blog b WHERE b.type_id=bt.type_id) AS num', false)->from('t_blog_type bt');
+        $this->db->where('bt.user_id', $user_id)->order_by('bt.order_num', "ASC");
+        return $this->db->get()->result();
+    }
+
+    // 插入一条博客类型信息
+    public function save_blog_type($type_name, $user_id, $order_num){
+        $this->db->insert('t_blog_type', array(
+            "type_name" => $type_name,
+            "user_id" => $user_id,
+            "order_num" => $order_num,
+        ));
         return $this->db->affected_rows();
     }
 }
