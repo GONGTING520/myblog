efca1abd3c2b8a0860bc7415d0f2909bcc286720
GONGTING520 <954160311@qq.com>
2018-04-06 19:40:15 +0800

    新增未登录的情况下：显示一条微博详情页；显示一个传入的user_id的全部博客列表的方法

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -44,6 +44,60 @@ class Welcome extends CI_Controller {
 		));
 	}
 
+	// 显示一条微博详情页
+    public function blog_detail(){
+		$blog_id = $this->input->get('blogId');
+		$user_id = $this->input->get('userId');
+		$this->load->model('blog_model');		
+        // 查询博客信息
+        $res = $this->blog_model->find_blog_by_blog_id($blog_id);
+        // 查询评论信息
+		$comments = $this->blog_model->find_comment_by_blog_id($blog_id);
+		$this->load->model('user_model');
+		$user = $this->user_model->find_by_user_id($user_id);
+        if($res){
+            $this->load->view('one_blog_detail', array(
+                "blog" => $res,
+				"comments" => $comments,
+				"user" => $user
+            ));
+        }else{
+            $this->load->view('index.html');
+        }
+    }
+
+	// 显示一个传入的user_id的全部博客列表
+	public function blogs($user_id)
+	{
+		$this->load->model('user_model');
+		$user = $this->user_model->find_by_user_id($user_id);
+
+		// 查找文章		
+		$this->load->model('blog_model');
+		$res = $this->blog_model->find_blog_by_user_id($user_id);
+
+		// 分页配置
+		$this->load->library('pagination');
+		$config['base_url'] = base_url().'welcome/blogs/'.$user_id;
+		$config['total_rows'] = count($res);
+		$config['per_page'] = 3;
+		$config['first_link'] = '首页';
+		$config['last_link'] = '尾页';
+		$config['next_link'] = '下一页';
+		$config['prev_link'] = '上一页';
+		$this->pagination->initialize($config);
+		$link = '<div class="page">'.$this->pagination->create_links().'</div>';
+
+		// 调用分页方法
+		// $this->uri->segment(4)指获取基础路径后面通过“/”分割后的第4个值
+		$rows = $this->blog_model->pagination_blog_by_user_id($user_id, $this->uri->segment(4), $config['per_page']);
+		
+		$this->load->view('one_blog_list', array(
+			"blogs" => $rows,
+			"link" => $link,
+			"user" => $user
+		));
+	}
 	public function login()
 	{
 		$this->load->view('login');
