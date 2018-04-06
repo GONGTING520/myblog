0c79fe39f2ed18eb2ab6f161b09387521e0842f5
GONGTING520 <954160311@qq.com>
2018-04-06 21:00:47 +0800

    新增根据user_id查找发给用户的所有评论，删除一条评论信息，须传入comment_id的方法

--- application/controllers/Comment.php
+++ application/controllers/Comment.php
@@ -21,4 +21,27 @@ class Comment extends CI_Controller {
             echo "fail";
         }
     }
+
+    // 根据user_id查找发给用户的所有评论
+    public function blog_comments($user_id){
+        $res = $this->comment_model->find_by_user_id($user_id);
+        if($res){
+            $this->load->view('blog_comments', array(
+                "comments" => $res
+            ));
+        }else{
+            $this->load->view('index.html');
+        }
+    }
+
+    // 删除一条评论信息，须传入comment_id
+    public function delete_comment(){
+        $comment_id = $this->input->get('commentId');
+        $rows = $this->comment_model->delete_comment_by_comment_id($comment_id);
+        if($rows > 0){
+            echo "success";
+        }else{
+            echo "fail";
+        }
+    }
 }
