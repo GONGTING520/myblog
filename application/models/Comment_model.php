bd124866a71b9fc5bb04fc8337d1a1cb687e8ad6
GONGTING520 <954160311@qq.com>
2018-04-06 21:00:16 +0800

    增加根据user_id查找所有发送给自己的评论，根据comment_id删除一条评论信息的方法

--- application/models/Comment_model.php
+++ application/models/Comment_model.php
@@ -12,4 +12,22 @@ class Comment_model extends CI_Model{
         ));
         return $this->db->affected_rows();
     }
+
+    // 根据user_id查找所有发送给自己的评论
+    public function find_by_user_id($user_id){
+        $this->db->select('c.*, b.title, b.blog_id, u.username');
+        $this->db->from('t_blog_type bt');
+        $this->db->join('t_blog b', "bt.type_id=b.type_id");
+        $this->db->join('t_comment c', "b.blog_id=c.blog_id");
+        $this->db->join('t_user u', "u.user_id=c.user_id");
+        $this->db->where('bt.user_id', $user_id);
+        $this->db->order_by('c.post_time', 'DESC');
+        return $this->db->get()->result();
+    }
+
+    // 根据comment_id删除一条评论信息
+    public function delete_comment_by_comment_id($comment_id){
+        $this->db->delete('t_comment', array("comment_id"=>$comment_id));
+        return $this->db->affected_rows();
+    }
 }
