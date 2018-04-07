f2e6e8c7f7b72a62d41fe0ac9bf5a4b9d2b0acdd
GONGTING520 <954160311@qq.com>
2018-04-07 22:24:01 +0800

    增加根据user_id查找所有发送给自己的评论，然后分页的数据库方法

--- application/models/Comment_model.php
+++ application/models/Comment_model.php
@@ -25,6 +25,19 @@ class Comment_model extends CI_Model{
         return $this->db->get()->result();
     }
 
+    // 根据user_id查找所有发送给自己的评论,分页
+    public function pagination_by_user_id($user_id, $offset, $page_length){
+        $this->db->select('c.*, b.title, b.blog_id, u.username');
+        $this->db->from('t_blog_type bt');
+        $this->db->join('t_blog b', "bt.type_id=b.type_id");
+        $this->db->join('t_comment c', "b.blog_id=c.blog_id");
+        $this->db->join('t_user u', "u.user_id=c.user_id");
+        $this->db->where('bt.user_id', $user_id);
+        $this->db->order_by('c.post_time', 'DESC');
+        $this->db->limit($page_length, $offset);
+        return $this->db->get()->result();
+    }
+
     // 根据comment_id删除一条评论信息
     public function delete_comment_by_comment_id($comment_id){
         $this->db->delete('t_comment', array("comment_id"=>$comment_id));
