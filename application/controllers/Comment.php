6b424fff49b7098a9208400a591e0122bd31fed7
GONGTING520 <954160311@qq.com>
2018-04-09 16:28:39 +0800

    给根据user_id查找发给用户的所有评论的方法增加分页功能

--- application/controllers/Comment.php
+++ application/controllers/Comment.php
@@ -25,13 +25,28 @@ class Comment extends CI_Controller {
     // 根据user_id查找发给用户的所有评论
     public function blog_comments($user_id){
         $res = $this->comment_model->find_by_user_id($user_id);
-        if($res){
-            $this->load->view('blog_comments', array(
-                "comments" => $res
-            ));
-        }else{
-            $this->load->view('index.html');
-        }
+
+        // 分页配置
+		$this->load->library('pagination');
+		$config['base_url'] = base_url()."comment/blog_comments/$user_id";
+		$config['total_rows'] = count($res);
+		$config['per_page'] = 3;
+		$config['first_link'] = '首页';
+		$config['last_link'] = '尾页';
+		$config['next_link'] = '下一页';
+		$config['prev_link'] = '上一页';
+		$this->pagination->initialize($config);
+		$link = '<div class="page">'.$this->pagination->create_links().'</div>';
+
+		// 调用分页方法
+		$rows = $this->comment_model->pagination_by_user_id($user_id, $this->uri->segment(4), $config['per_page']);		
+		
+        var_dump($this->uri->segment(4));
+        $this->load->view('blog_comments', array(
+            "comments" => $rows,
+            "link" => $link,
+            "count" => count($res)
+        ));
     }
 
     // 删除一条评论信息，须传入comment_id
